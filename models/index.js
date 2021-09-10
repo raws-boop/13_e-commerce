39166ebc0b670140621a6bf7d68d5d7d08001168
Ross Holtrop <holtrop.ross@gmail.com>
2021-09-09 22:54:20 -0700

    model update

--- models/index.js
+++ models/index.js
@@ -1,3 +1,21 @@
+
+MikeAdu
+/
+13.5-E-Commerce-Back-End
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+More
+13.5-E-Commerce-Back-End/models/index.js /
+@MikeAdu
+MikeAdu added sequlize to index.js
+ History
+ 1 contributor
+28 lines (27 sloc)  668 Bytes
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
@@ -8,22 +26,18 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category,{
   foreignKey: "categoryId"
 });
-
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "productId"
-  });
-
+foreignKey: "productId"
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
 through:ProductTag, foreignKey: "productId"
 });
-
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through:ProductTag, foreignKey: "tagId"
-  });
-
+through:ProductTag, foreignKey: "tagId"
+});
 module.exports = {
   Product,
   Category,
